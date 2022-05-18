92e7018faa9d910650ec0110bb8b701040d91330
Dancan Chibole <dancankingstar@gmail.com>
2022-05-18 16:22:42 +0300

    crud operations commit

---
+++ src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.example.springbootbackend.controller;
+
+
+import com.example.springbootbackend.model.Employee;
+import com.example.springbootbackend.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    //create employee
+    @PostMapping
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    //get all employees
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    // get employee by id
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId){
+        return new ResponseEntity<>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
+    }
+
+    //update employee
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id, @RequestBody Employee employee){
+        return new ResponseEntity<>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+    }
+
+    //delete employee
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
+        //delete employee from DB
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<String>("Employee deleted successfully", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/springbootbackend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.springbootbackend.service.impl;
+
+import com.example.springbootbackend.exception.ResourceNotFoundException;
+import com.example.springbootbackend.model.Employee;
+import com.example.springbootbackend.repository.EmployeeRepository;
+import com.example.springbootbackend.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+//        Optional<Employee> employee = employeeRepository.findById(id);
+//        if(employee.isPresent()){
+//            return employee.get();
+//        } else{
+//            throw new ResourceNotFoundException("Employee", "Id", id);
+//        }
+        return employeeRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Employee", "Id", id));
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee, long id) {
+        //check whether the employee is existing in DB
+        Employee existingEmployee = employeeRepository.findById(id).orElseThrow(
+                ()->new ResourceNotFoundException("Employee", "Id", id));
+        existingEmployee.setFirstName(employee.getFirstName());
+        existingEmployee.setLastName(employee.getLastName());
+        existingEmployee.setEmail(employee.getEmail());
+        employeeRepository.save(existingEmployee);
+        return existingEmployee;
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+        employeeRepository.findById(id).orElseThrow(
+                ()->new ResourceNotFoundException("Employee", "Id", id));
+        employeeRepository.deleteById(id);
+    }
+}
